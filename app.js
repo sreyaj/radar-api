8091acb791e23f6dfc87bcee64afe7f9e6699e67
tahmidshahriar <tahmids@seas.upenn.edu>
2015-05-20 14:00:17 -0700

    Open and close issues visible

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var home = require('./routes/home');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/issues', routes);
+app.use('/', home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,72 @@
 var express = require('express');
-var router = express.Router();
-var request = require('request');
-//0.9.2
-var async = require('async');
-data = "";
-var titlesStore = [];
-var done = false;
-var resStore;
-var iStore = 1;
-/* GET home page. */
-var options = { 
-	url: 'https://api.github.com/repos/shippable/support/issues?page=1&per_page=100',
-	headers: {
-		'User-Agent': 'funapp'
-	}
-};
-
-router.get('/', function(req, res, next) {
-	titlesStore = [];
-	iStore = 1;
-	console.log(1);
-	done = false;
-	resStore = res;
-	options.url = "https://api.github.com/repos/shippable/support/issues?page=1&per_page=100";
-		/////////////////////////////////////////////////////////////////////////////////
-	parseGit();
-});
-
-module.exports = router;
-
-function parseGit() {
-	console.log(2);
-	/////////////////////////////////////////////////////////////////////////////////
-	request(options, storeTitle);
-}
-
-function turnPage() {
-	console.log(3);
-
-	if (titlesStore.length % 100 == 0 && titlesStore.length != 0 && done == false) {
-		iStore = iStore + 1;
-		addPage();
-	} else {
-		load();
-	}
-}
-
-function addPage(){
-	var tempOptions = options;
-	tempOptions.url = 'https://api.github.com/repos/shippable/support/issues?page=' 
-	+ iStore + '&per_page=100';
-	loadRequest(tempOptions);
-}
-
-
-
-function load() {
-	console.log(4);
-	resStore.render('index', { issues: titlesStore });
-}
-
-
-function loadRequest(opt) {
-	console.log(5);
-	/////////////////////////////////////////////////////////////////////////////////
-	request(opt, storeTitle);
-}
-
-function storeTitle (error, response, body) {
+	var router = express.Router();
+	var request = require('sync-request');
+	data = "";
+	issue_array=[];
+	var page=1;
+	var no_issues_mod100=0;
+	/* GET home page. */
 	
-	console.log(6); 
-	/////////////////////////////////////////////////////////////////////////////////
-	if (!error && response.statusCode == 200) {
-		data = (JSON.parse(body));
-		for (i = 0; i < data.length; i++) {
-			titlesStore.push(data[i].number);
+//// OPEN VAR
+//// CLOSE VAR
+
+
+	console.log("0");
+
+	router.get('/', function(req, res, next) {
+		res.redirect('/');
+	});
+
+	router.post('/', function(req, res, next) {
+		issue_array = [];
+		data = "";
+		if (req.body.state == "Open") {
+			mainLoading("open", req, res, next);	
+		} else {
+			mainLoading("closed", req, res, next);
 		}
-	} 
-	console.log(7);
-	if (data.length == 0) {
-		done = true;
+	});
+
+	function mainLoading(state, req, res, next) {
+		page = 1;
+		repo = req.body.name + "/" + req.body.repo;
+		console.log(repo);
+
+		while(data.length%100==0 && no_issues_mod100==0){
+			var options = { 
+				headers: {
+				'User-Agent': 'funapp'
+				}
+			};
+			var data_json=request('GET','https://api.github.com/repos/' + repo + '/issues?state=' + state + '&client_id=58161dcf40849abffecd&client_secret=10ee9d2f6a2402cdca283d8b2ba01529bb216475&page='+page+'&per_page=100',options);
+
+			data=JSON.parse(data_json.getBody());
+			for(i=0;i<data.length;i++){
+	  			issue_array.push(data[i].title);
+	  		}
+	  		if(data.length==0){
+				console.log("3");
+				no_issues_mod100=1;
+			}
+	  		page++;
+	  	}
+	  	
+		console.log(issue_array.length);
+		
+	  	load(res, state);
 	}
 
-	turnPage();
-}
+
+	module.exports = router;
+
+
+	function load(res, state) {
+		if (state == "open") {
+			res.render('index', { title: issue_array, state: "Open issues" });
+		} else {
+			res.render('close', { title: issue_array, state: "Closed issues" });
+		}
+	}
+
+
+	
