6b0be345a80d826556d94bc483a364b90aacaf97
kailevy <kai0levy@gmail.com>
2015-06-05 14:20:20 -0700

    another simple test

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ describe("Index", function() {
 
 describe("Issues page", function() {
   it("renders a page with an issues table", function(done) {
-    superagent.get("localhost:3000/issues?username=shippable&repo=support&token=47df5e1408f4105cfb764e7c0f5cd19bc7a5aa95&days=2&daysEnd=5&state=Open")
+    superagent.get("localhost:3000/issues?username=shippable&repo=support&token=47df5e1408f4105cfb764e7c0f5cd19bc7a5aa5&days=2&daysEnd=5&state=Open")
     .end(function(err, res) {
       (err === null).should.equal(true);
       res.text.should.startWith("<!DOCTYPE html>\n<html>\n  <head>\n    <title>Issues</title>");
